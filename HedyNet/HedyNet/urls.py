180e8eb0fc2a7396b1b471df1934066310df54ad
Audra Johnson <audrakjohnson@gmail.com>
2013-07-10 22:01:27 +0000

    Start an othersites page
    This patch:
    
    * Moves the twitter embed to an embeds directory in templates
    * Adds a LibraryThing template in the embeds directory in templates
    * Starts an othersites page that has sections for our other sites
    
    Todo still:
    
    * Abstract out into a model, so people can easily change/modify which sites
    * Make it so that OtherSite objects can also have their own individual pages

--- HedyNet/HedyNet/urls.py
+++ HedyNet/HedyNet/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^tos$', TemplateView.as_view(template_name="tos.html"), name="tos"),
     url(r'^donations$', TemplateView.as_view(template_name="donations.html"), name="donations"),
     url(r'^visit$', TemplateView.as_view(template_name="visit.html"), name="visit"),
+    url(r'^othersites$', TemplateView.as_view(template_name="othersites.html"), name="othersites"),
 
     url(r'^faq/', include('FAQ.urls')),
 
