a233fce6b675de083187e81d2826afdc8134e836
Audra Johnson <audrakjohnson@gmail.com>
2014-05-18 22:01:07 +0000

    Don't automatically use the debug toolbar for local development

--- HedyNet/HedyNet/settings/local.py
+++ HedyNet/HedyNet/settings/local.py
@@ -49,7 +49,7 @@ CACHES = {
 ########## TOOLBAR CONFIGURATION
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
-    'debug_toolbar',
+#    'debug_toolbar',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
@@ -57,6 +57,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 ########## END TOOLBAR CONFIGURATION
