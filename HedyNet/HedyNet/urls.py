007fc3c2ea00e7fe7dd58bee33847ad5afe7ed57
Audra Johnson <audrakjohnson@gmail.com>
2014-05-18 21:44:14 +0000

    applications module not ready as a URL import

--- HedyNet/HedyNet/urls.py
+++ HedyNet/HedyNet/urls.py
@@ -32,7 +32,7 @@ urlpatterns = patterns('',
     (r'^profiles/', include('profiles.urls')),
     (r'^payments/', include('payments.urls')),
     (r'^othersites/', include('othersites.urls')),
-    (r'^applications/', include('applications.urls')),
+    #(r'^applications/', include('applications.urls')),
     
     # Examples:
     # url(r'^$', 'HedyNet.views.home', name='home'),
